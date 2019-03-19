4d958723298721ea3165bf1e58e51864816a33ae
wyb <wangyabin@vue.video>
2019-03-20 01:13:08 +0800

    update

--- model/db.go
+++ model/db.go
@@ -10,7 +10,7 @@ var db *gorm.DB
 
 func setupDB() {
     var err error
-    db, err = gorm.Open("mysql", "root:248536@tcp(127.0.0.1:3306)/go?charset=utf8&parseTime=True&loc=Asia%2fShanghai")
+    db, err = gorm.Open("mysql", "go:password@tcp(127.0.0.1:3306)/go?charset=utf8&parseTime=True&loc=Asia%2fShanghai")
     if nil != err {
         panic(err)
     }
@@ -18,6 +18,12 @@ func setupDB() {
         if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8").CreateTable(&entity.User{}).Error; err != nil {
             panic(err)
         }
+    }
+    if !db.HasTable(&entity.Post{}) {
+        if err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8").CreateTable(&entity.Post{}).Error; err != nil {
+            panic(err)
+        }
+        db.Model(&entity.Post{}).AddIndex("idx_user", "uid", "valid", "created_at")
     }
     db.LogMode(true)
 }
